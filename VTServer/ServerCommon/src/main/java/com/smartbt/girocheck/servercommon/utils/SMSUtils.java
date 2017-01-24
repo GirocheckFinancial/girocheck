ecbfea335de2d39317f096dc298a99355dcd6ce8
sreekanthki <sreekanth.kunnathur@girmiti.com>
2017-01-24 17:27:34 +0530

    Committed for reading SMS settings from System.property

--- VTServer/ServerCommon/src/main/java/com/smartbt/girocheck/servercommon/utils/SMSUtils.java
+++ VTServer/ServerCommon/src/main/java/com/smartbt/girocheck/servercommon/utils/SMSUtils.java
@@ -1,4 +1,4 @@
- package com.smartbt.girocheck.servercommon.utils;
+package com.smartbt.girocheck.servercommon.utils;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -18,19 +18,23 @@ import org.apache.http.util.EntityUtils;
  */
 public class SMSUtils {
     
-    static String url = "https://go4clients.com:8443/TelintelSms/api/sms/sendmessage";
+    //static String url = "https://go4clients.com:8443/TelintelSms/api/sms/sendmessage"; //http://go4clients.com:3011/TelintelSms/api/sms/sendmessage
     
-    static String API_KEY="aec51d9fc4a64bd19639b5b2c2a88e5d";
+    //static String API_KEY="aec51d9fc4a64bd19639b5b2c2a88e5d";
     
-    static String API_SECRET="0086572281";
+    //static String API_SECRET="0086572281";
  
     public static void sendSMS(String phone, String message) {
-       
+        
+        String url = System.getProperty("SMS_URL");
+        String API_KEY = System.getProperty("SMS_API_KEY");
+        String API_SECRET = System.getProperty("SMS_API_SECRET");
+        
         HttpPost postRequest = new HttpPost(url);
         ObjectMapper mapper = new ObjectMapper();
  
         try {
-            System.out.println("came here");
+            System.out.println("******** CAME HERE ******************");
  
             String request = "{\"toList\" :[\""
                     + phone
@@ -46,14 +50,14 @@ public class SMSUtils {
             StringEntity input = new StringEntity(request);
             input.setContentType("application/json");
             postRequest.setEntity(input);
-            System.out.println("came here before execute");
+            System.out.println("********* CAME HERE BEFORE SENDING SMS*******************");
  
             HttpResponse response = httpClient.execute(postRequest);
             HttpEntity responseEntity = response.getEntity();
             
             if(responseEntity!=null) {
                String resp = EntityUtils.toString(responseEntity);
-               System.out.println("came here after execute :- "+resp);// mapper.readValue(response.getEntity().getContent(), Response.class);
+               System.out.println("********* SMS SEND SUCCESSFULLY*******************");// mapper.readValue(response.getEntity().getContent(), Response.class);
             } 
             
  
@@ -69,9 +73,6 @@ public class SMSUtils {
     }
      
     public static void main(String s[]){
-        sendSMS("917204881557","Thank you for using VoltCash");
+        sendSMS("919886730064","Thank you for using VoltCash");
     }
 }
-    
- 
-

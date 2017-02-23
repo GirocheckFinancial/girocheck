0a1d38edd4fd24db54262c1de2ecb32c6d3de384
sreekanthki <sreekanth.kunnathur@girmiti.com>
2017-02-23 12:14:44 +0530

    Committed for changing the url for iStream2 integration.

--- VTServer/iStream2/iStream2-ejb/src/main/java/com/smartbt/vtsuite/manager/IStream2BusinessLogic.java
+++ VTServer/iStream2/iStream2-ejb/src/main/java/com/smartbt/vtsuite/manager/IStream2BusinessLogic.java
@@ -62,7 +62,8 @@ public class IStream2BusinessLogic {
             service = new TLS_Service();
             port = service.getTLSPort();
 
-            url = "https://stable-1.istreamdeposit.com/TLS?wsdl";
+          // url = "https://stable-1.istreamdeposit.com/TLS?wsdl";
+             url = "https://stable-1.istreamdeposit.com/TLS";
 
 //            if(url == null){
 //                url = "https://bizsrv.tcmsystem.net/IStreamWS/iStreamSrvHost.asmx?WSDL";

37e762e2d3495cd779e4637b08b9bee13b5b8542
sreekanthki <sreekanth.kunnathur@girmiti.com>
2017-03-30 16:14:13 +0530

    Committed for IRP-17:- Issues in IStream2 module

--- VTServer/FrontAMS/src/main/java/com/smartbt/vtsuite/manager/CheckResendManager.java
+++ VTServer/FrontAMS/src/main/java/com/smartbt/vtsuite/manager/CheckResendManager.java
@@ -6,23 +6,16 @@ import com.smartbt.girocheck.servercommon.display.message.BaseResponse;
 import com.smartbt.girocheck.servercommon.display.message.ResponseData;
 import com.smartbt.girocheck.servercommon.display.message.ResponseDataList;
 import com.smartbt.girocheck.servercommon.enums.ParameterName;
-import com.smartbt.girocheck.servercommon.enums.ResultCode;
-import com.smartbt.girocheck.servercommon.enums.ResultMessage;
 import com.smartbt.girocheck.servercommon.enums.TransactionType;
-import com.smartbt.girocheck.servercommon.jms.JMSManager;
 import com.smartbt.girocheck.servercommon.messageFormat.DirexTransactionRequest;
 import com.smartbt.girocheck.servercommon.messageFormat.DirexTransactionResponse;
 import com.smartbt.girocheck.servercommon.model.Check;
 import com.smartbt.girocheck.servercommon.utils.CustomeLogger;
 import com.smartbt.vtsuite.vtcommon.Constants;
-import java.io.Serializable;
+import java.text.DecimalFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.ObjectMessage;
 
 /**
  *
@@ -30,9 +23,7 @@ import javax.jms.ObjectMessage;
  */
 public class CheckResendManager {
 
-    private CheckResendDAO checkResendDAO = CheckResendDAO.get();
-    private JMSManager jmsManager = JMSManager.get();
-    String correlationId;
+    private CheckResendDAO checkResendDAO = CheckResendDAO.get();    
     protected static CheckResendManager _this;
 
     public static CheckResendManager get() {
@@ -52,54 +43,51 @@ public class CheckResendManager {
         String checkId = String.valueOf(id);
         DirexTransactionRequest direxTransactionRequest = new DirexTransactionRequest();
         CustomeLogger.Output(CustomeLogger.OutputStates.Info, "[CheckResendManager] processTransaction " + checkId, null);
-        
+
         try {
             Check check = checkResendDAO.getCheckDetails(id);
             Map map = new HashMap();
             map.put(TransactionType.TRANSACTION_TYPE, TransactionType.ISTREAM2_SEND_SINGLE_ICL);
             String prodProperty = System.getProperty("PROD");
             Boolean isProd = prodProperty != null && prodProperty.equalsIgnoreCase("true");
-            if(isProd)
-            {
-              String userName = System.getProperty("ISTREAM2_USER_NAME");
-              String password = System.getProperty("ISTREAM2_PWD");
+            if (isProd) {
+                String userName = System.getProperty("ISTREAM2_USER_NAME");
+                String password = System.getProperty("ISTREAM2_PWD");
+                map.put(ParameterName.USER, userName);
+                map.put(ParameterName.PASSWORD, password);
 
-               map.put(ParameterName.USER, userName);
-               map.put(ParameterName.PASSWORD, password);
-               
-            }else{
-                 map.put(ParameterName.USER, "GCTLS");
-                 map.put(ParameterName.PASSWORD, "sts283");
-                 map.put(ParameterName.DEPOSIT, "4pm Deposit");
-
+            } else {
+                map.put(ParameterName.USER, "GCTLS");
+                map.put(ParameterName.PASSWORD, "sts283");
+                map.put(ParameterName.DEPOSIT, "4pm Deposit");
             }
-            // map.put(ParameterName.USER, "GCTLS");
-            //map.put(ParameterName.PASSWORD, "sts283");
-            //map.put(ParameterName.LOCATION_ID, "4769778");
-            //map.put(ParameterName.DEPOSIT, "4pm Deposit");
             map.put(ParameterName.LOCATION_ID, check.getLocationId());
-            map.put(ParameterName.AMMOUNT, check.getTransaction().getAmmount());
-            map.put(ParameterName.MICR, check.getMicr());
+            
+            DecimalFormat twoPlaces = new DecimalFormat("0.00");
+            String amountString = twoPlaces.format(check.getTransaction().getAmmount());            
+            
+            String originalMICR = check.getMicr();
+            String translatedMICR = translateMICR(originalMICR);           
+            
+            map.put(ParameterName.AMMOUNT, amountString);
+            map.put(ParameterName.MICR, translatedMICR);
             map.put(ParameterName.CHECK_ID, check.getId());
             map.put(ParameterName.CHECK_FRONT, check.getCheckFront());
-            map.put(ParameterName.CHECK_BACK, check.getCheckBack());            
-            
+            map.put(ParameterName.CHECK_BACK, check.getCheckBack());
+
             direxTransactionRequest.setTransactionData(map);
             direxTransactionRequest.setCorrelation(checkId);
-            direxTransactionRequest.setTransactionType(TransactionType.ISTREAM2_SEND_SINGLE_ICL);
+            direxTransactionRequest.setTransactionType(TransactionType.ISTREAM2_SEND_SINGLE_ICL);            
 
-            correlationId = checkId;
+            DirexTransactionResponse iStream2Response = IStream2HostManager.get().processTransaction(direxTransactionRequest);
 
-            long wait_time = 1800000;//30 mins
+            CustomeLogger.Output(CustomeLogger.OutputStates.Info, "iStream2Response " + iStream2Response, null);
 
-            DirexTransactionResponse iStreamResponse = sendMessageToHost(direxTransactionRequest, "ISTREAM2", wait_time, false);
-            CustomeLogger.Output(CustomeLogger.OutputStates.Info, "iStreamResponse " +iStreamResponse, null);         
-            
-            if (iStreamResponse != null && iStreamResponse.wasApproved()) {
-                CustomeLogger.Output(CustomeLogger.OutputStates.Info, "iStreamResponse.wasApproved()" +iStreamResponse.wasApproved(), null);
+            if (iStream2Response != null && iStream2Response.wasApproved()) {
+                CustomeLogger.Output(CustomeLogger.OutputStates.Info, "iStream2Response.wasApproved()" + iStream2Response.wasApproved(), null);
                 CheckDisplay checkDisplay = checkResendDAO.resendCheck(id);
-                CustomeLogger.Output(CustomeLogger.OutputStates.Info, "iStreamResponse.wasApproved() = true" +checkDisplay, null);
-                if (checkDisplay != null) {                  
+                CustomeLogger.Output(CustomeLogger.OutputStates.Info, "iStream2Response.wasApproved() = true" + checkDisplay, null);
+                if (checkDisplay != null) {
                     response.setStatus(Constants.CODE_SUCCESS);
                     response.setStatusMessage(com.smartbt.girocheck.common.VTSuiteMessages.SUCCESS);
                     CustomeLogger.Output(CustomeLogger.OutputStates.Info, "[CheckResendManager] processTransaction finished" + checkId, null);
@@ -120,43 +108,10 @@ public class CheckResendManager {
         return response;
     }
 
-    private DirexTransactionResponse sendMessageToHost(DirexTransactionRequest request, String hostName, long waitTime, boolean notifyToIstream) throws JMSException, Exception {
-        CustomeLogger.Output(CustomeLogger.OutputStates.Info, "[CheckResendManager] Send message to host " + hostName, null);
-
-        Properties props = new Properties();
-        props.setProperty("hostName", hostName);
-
-        jmsManager.sendWithProps(request, jmsManager.getHostInQueue(), correlationId, props);
-
-        if (request.getTransactionType() == TransactionType.ISTREAM2_SEND_SINGLE_ICL) {
-            return receiveMessageFromHost(request.getTransactionType(), hostName, waitTime, notifyToIstream);
-        } else {
-            return null;
+    private String translateMICR(String originalMICR) {
+        if (originalMICR != null) {
+            return originalMICR.replace("C", "<").replace("A", ":").replace("D", "=");
         }
-    }
-
-    private DirexTransactionResponse receiveMessageFromHost(TransactionType transactionType, String hostName, long waitTime, boolean notifyToIstream) throws Exception {
-        CustomeLogger.Output(CustomeLogger.OutputStates.Info, "[CheckResendManager] Receiving message from host " + hostName, null);
-
-        Message message = null;
-        DirexTransactionResponse response;
-        try {
-            message = jmsManager.receive(jmsManager.getHostOutQueue(), correlationId, waitTime);
-        } catch (Exception e) {
-            CustomeLogger.Output(CustomeLogger.OutputStates.Info, "[CheckResendManager] receiving message on receiveMessageFromHost() throws an exeption.", null);
-            response = DirexTransactionResponse.forException(transactionType, ResultCode.RESPONSE_TIME_EXCEEDED, ResultMessage.RESPONSE_TIME_EXCEEDED, " " + hostName);
-            response.setTransactionType(transactionType);
-
-            throw new Exception();
-        }
-
-
-        ObjectMessage objectMessage = (ObjectMessage) message;
-        Serializable s = objectMessage.getObject();
-        response = (DirexTransactionResponse) s;
-
-        CustomeLogger.Output(CustomeLogger.OutputStates.Info, "[CheckResendManager] Message received", null);
-
-        return response;
+        return "";
     }
 }
